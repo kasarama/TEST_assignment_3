91b8fc88f8a7b0787b4db330d01844d82f3111fd
kasarama <magdalena.wawrzak@gmail.com>
2022-10-20 11:59:56 +0200

    implement migration

---
+++ src/main/resources/db/migration/V2__InsertCustomers.sql
@@ -0,0 +1,9 @@
+insert into customers
+(birthdate, first_name, last_name)
+values
+    ('1988-04-11', 'Mark', 'Wash'),
+    ('2001-07-31', 'Thomas', 'Hough'),
+    ('1967-01-13', 'Maria', 'Lee'),
+    ('1973-12-01', 'John', 'Baldo'),
+    ('1986-08-19', 'Hanna', 'Puck')
+;
